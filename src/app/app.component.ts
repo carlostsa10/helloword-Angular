fd0d0d4fb3131fd7c4781b908b9d7a585ceb50bb
Carlos Tavares <carlostsa10@gmail.com>
2023-05-29 11:40:32 -0300

    aprendendo sobre forma de entrada de dados

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'hello-world';
+
+  public destruir: boolean = true;
+  public addValue: number = 0;
+
+  constructor() { }
+
+  public add(){
+    this.addValue += 2
+  }
+  ngOnInit(): void {
+    
+  }
 }
